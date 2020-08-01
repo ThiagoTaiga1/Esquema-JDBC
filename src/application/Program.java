39e4b22f7c808b765ab3dfc23799c0f09263f9a8
ThiagoTaiga1 <68219703+ThiagoTaiga1@users.noreply.github.com>
2020-08-01 10:35:23 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.sql.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Department obj = new Department (1 ,"books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21,"Ana","anacarola@gmail.com",new Date(0),300.0,obj);
+				
+		System.out.println(seller);
+		
 	}
 
 }
