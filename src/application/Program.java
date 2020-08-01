e81593df6a580ec04f79fe1e1e2d789abecd8e25
ThiagoTaiga1 <68219703+ThiagoTaiga1@users.noreply.github.com>
2020-08-01 11:07:11 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.sql.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,7 +14,9 @@ public class Program {
 		Department obj = new Department (1 ,"books");
 		
 		Seller seller = new Seller(21,"Ana","anacarola@gmail.com",new Date(0),300.0,obj);
-				
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 		
 	}
--- src/model/dao/DepartementDao.java
+++ src/model/dao/DepartementDao.java
@@ -1,5 +1,16 @@
 package model.dao;
 
-public interface DepartementDao {
+import java.util.List;
 
+import model.entities.Department;
+
+public interface DepartementDao {
+	//INSERIR,UPDATE,DELETE OBJETO NO BD , COMO PARAMETRO DE ENTRADA !
+		void insert (Department obj);
+		void update (Department obj);
+		void deleteByIdd(Integer id);
+		//PEGAR ID , CONSULTAR OBJ COM ESSE ID !
+		Department findById(Integer id);
+		//RETORNAR TODOS OS DEPARTAMENTOS !
+		List<Department> findAll();
 }
