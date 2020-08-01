d3cb6c243d9ea4d2b68394b1857531c3bc634a16
ThiagoTaiga1 <68219703+ThiagoTaiga1@users.noreply.github.com>
2020-08-01 10:52:14 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+
+	//INSERIR,UPDATE,DELETE OBJETO NO BD , COMO PARAMETRO DE ENTRADA !
+	void insert (Seller obj);
+	void update (Seller obj);
+	void deleteByIdd(Integer id);
+	//PEGAR ID , CONSULTAR OBJ COM ESSE ID !
+	Seller findById(Integer id);
+	//RETORNAR TODOS OS DEPARTAMENTOS !
+	List<Seller> findAll();
+}
+
+
